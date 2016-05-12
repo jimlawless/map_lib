383fa336c69af64c82a6156a55053d60667e6928
Jim Lawless <jimbo@radiks.net>
2016-05-12 18:13:38 -0500

    Update map_lib.h

--- map_lib.h
+++ map_lib.h
@@ -2,44 +2,47 @@
 // A simple associative-array library for C
 //
 // License: MIT / X11
-// Copyright (c) 2009, 2015 by James K. Lawless
-// jimbo@radiks.net http://jiml.us
-// See: http://www.mailsend-online.com/license2015.php
+// Copyright (c) 2009, 2012 by James K. Lawless
+// jimbo@radiks.net http://www.radiks.net/~jimbo
+// http://www.mailsend-online.com
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
 
 #ifndef MAP_LIB_H
 #define MAP_LIB_H
 
-typedef enum {
-   MAP_OF_STRINGS=0,
-   MAP_OF_INTS,
-   MAP_OF_STRUCTS
-} map_type ;
-
 struct map_t {
    struct map_t *nxt;
    char *name;
-   map_type tag;
-   union {
-      char *value;
-      int intValue;
-      void *structValue;
-   } v;
+   void *value;
 } ;
 
 
 struct map_t *map_create();
-void map_free(struct map_t *map);
 void map_set(struct map_t *m,char *name,char *value);
 char *map_get(struct map_t *m,char *name);
-
-struct map_t *map_int_create();
-void map_int_set(struct map_t *m,char *name, int intValue);
-int map_int_get(struct map_t *m,char *name);
-
-struct map_t *map_struct_create();
-void map_struct_set(struct map_t *m,char *name, void *structValue);
-void *map_struct_get(struct map_t *m,char *name);
-
-
+void *map_get_ptr(struct map_t *m,char *name);
+void map_set_ptr(struct map_t *m,char *name, void *value);
+void map_free_strings(struct map_t *m);
 #endif
  
