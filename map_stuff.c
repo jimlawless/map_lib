d64fafc79c5e0ca243d48d378d92cd93231f1425
Jim Lawless <lawless.jim@gmail.com>
2018-09-26 19:41:30 -0500

    Update map_stuff.c
    Restored source code to working order.

--- map_stuff.c
+++ map_stuff.c
@@ -1,31 +1,15 @@
 #include <stdio.h>
 #include "map_lib.h"
 
-
 void display_both(struct map_t *m,char *key);
-void test_strings();
-void test_ints();
-void test_structs();
-
-
-
 
 int main(int argc,char **argv) {
-   test_strings();
-   test_ints();
-   test_structs();
-}
-   
-void test_strings() {   
    struct map_t *test;
 
-   printf("\nExercising maps of strings...\n");
-   printf("------\n");
    test=map_create();
    map_set(test,"One","Won");
    map_set(test,"Two","Too");
    map_set(test,"Four","Fore");
-
 
       // display them out of order
    display_both(test,"Two");
@@ -52,16 +36,7 @@ void test_strings() {
    display_both(test,"Eight");
 }
 
-void test_ints() {
-   printf("\nExercising maps of ints...\n");
-   printf("------\n");
-}
-
-void test_structs() {
-   printf("\nExercising maps of structs...\n");
-   printf("------\n");
-}
-
 void display_both(struct map_t *m,char *first) {
    printf("%s %s\n",first,map_get(m,first));
 }
+
